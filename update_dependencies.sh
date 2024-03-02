e32bdc3daf77866f916d54b35c8ffd68f662fc66
supermarine1377 <supermarine@outlook.jp>
2024-03-02 20:32:33 +0900

    Fix lint error

--- update_dependencies.sh
+++ update_dependencies.sh
@@ -8,40 +8,40 @@ NC='\033[0m' # No Color
 colorize() {
     local color="$1"
     shift
-    echo -e "${color}$@${NC}"
+    echo -e "${color}$*${NC}"
 }
 
 update_brew_dependencies() {
-    echo -e "$(colorize $GREEN 'Updating brew dependencies')"
+    echo -e "$(colorize "$GREEN" 'Updating brew dependencies')"
     brew bundle dump --force || {
-        echo -e "$(colorize $RED 'Error updating brew dependencies')"
+        echo -e "$(colorize "$RED" 'Error updating brew dependencies')"
         exit 1
     }
     mv Brewfile brew
-    echo -e "$(colorize $GREEN 'Updated brew dependencies successfully')"
+    echo -e "$(colorize "$GREEN" 'Updated brew dependencies successfully')"
 }
 
 copy_zsh_configurations() {
-    echo -e "$(colorize $GREEN 'Copying zsh configurations')"
+    echo -e "$(colorize "$GREEN" 'Copying zsh configurations')"
     cp ~/.zshrc zsh/ || {
-        echo -e "$(colorize $RED 'Error copying zsh configurations')"
+        echo -e "$(colorize "$RED" 'Error copying zsh configurations')"
         exit 1
     }
     cp ~/.zprofile zsh/ || {
-        echo -e "$(colorize $RED 'Error copying zsh configurations')"
+        echo -e "$(colorize "$RED" 'Error copying zsh configurations')"
         exit 1
     }
-    echo -e "$(colorize $GREEN 'Copied zsh configurations successfully')"
+    echo -e "$(colorize "$GREEN" 'Copied zsh configurations successfully')"
 }
 
 update_vscode_configurations() {
-    echo -e "$(colorize $GREEN 'Updating vscode configurations')"
+    echo -e "$(colorize "$GREEN" 'Updating vscode configurations')"
     cp "$HOME/Library/Application Support/Code/User/settings.json" vscode/ || {
-        echo -e "$(colorize $RED 'Error updating vscode configurations')"
+        echo -e "$(colorize "$RED" 'Error updating vscode configurations')"
         exit 1
     }
     code --list-extensions > vscode/extensions.txt
-    echo -e "$(colorize $GREEN 'Updated vscode configurations successfully')"
+    echo -e "$(colorize "$GREEN" 'Updated vscode configurations successfully')"
 }
 
 main() {
@@ -49,7 +49,7 @@ main() {
     copy_zsh_configurations
     update_vscode_configurations
 
-    echo -e "$(colorize $GREEN 'Updated all dependencies successfully')"
+    echo -e "$(colorize "$GREEN" 'Updated all dependencies successfully')"
 }
 
 main
